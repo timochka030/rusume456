4cab300c5f16ca04f8dc095aadf337e305baa3fd
timochka030 <timofijkisil030@gmail.com>
2023-07-27 15:04:05 +0300

    +++

--- src/route/index.js
+++ src/route/index.js
@@ -12,7 +12,9 @@ router.get('/', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('index', {})
+  res.render('index', {
+    layout: '456',
+  })
   //                  ↑↑ сюди вводимо JSON дані
 })
 
@@ -27,6 +29,37 @@ router.get('/summary', function (req, res) {
 })
 
 // ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/settings', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('settings', {
+    layout: '456',
+    // ↙ сюди вводимо JSON дані
+  })
+})
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/testy', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('testy', {
+    layout: '456',
+    // ↙ сюди вводимо JSON дані
+  })
+})
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/sign', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('sign', {
+    layout: '456',
+    // ↙ сюди вводимо JSON дані
+  })
+})
 
 // Підключаємо роутер до бек-енду
 module.exports = router
